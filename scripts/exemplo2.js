23446bb44e4a72983926bccf9f5948e9ed32fc31
Marcos Faino <marcos.faino@ifro.edu.br>
2023-05-16 21:13:49 -0400

    exemplos javascript

---
+++ scripts/exemplo2.js
@@ -0,0 +1,15 @@
+var n1 = parseInt(prompt('Informe o primeiro valor:'));
+var n2 = parseInt(prompt('Informe o segundo valor:'));
+
+// parseFloat() para convertar para números reais.
+
+// alert(n1+n2);
+
+result.innerHTML = n1+n2;
+
+
+var resp = confirm("Podemos continuar?");
+
+document.write("<p>" + resp + "</p>");
+
+//console.log(n1+n2);
